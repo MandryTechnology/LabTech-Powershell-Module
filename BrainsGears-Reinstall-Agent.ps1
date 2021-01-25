eb6ced7f97e38ac2a128d00298c8ab0aa814e6d4
Joel <68925180+sm00chdog@users.noreply.github.com>
2021-01-25 11:04:46 -0600

    Create BrainsGears-Reinstall-Agent.ps1

---
+++ BrainsGears-Reinstall-Agent.ps1
@@ -0,0 +1,14 @@
+Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Braingears/PowerShell/master/Automate-Module.psm1');
+$server = 'support.mandrytechnology.com'
+$token = 'c3d6126258255ea5f5beb09b6382e4d5'
+
+Confirm-Automate
+
+If ($Automate.LocationID) {
+	$location = $Automate.LocationID
+}
+else {
+	$location = '1'
+}
+
+Install-Automate -Server $server -LocationID $location -Token $token -Transcript
